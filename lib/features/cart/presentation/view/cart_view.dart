43a8c538aeab69efb2fb4b0a2ac1d89440b4f311
Aayush kafle <aayushkafle192@gmail.com>
2025-08-03 18:43:00 +0545

    Update cart view logic/layout

--- lib/features/cart/presentation/view/cart_view.dart
+++ lib/features/cart/presentation/view/cart_view.dart
@@ -90,7 +90,7 @@ class _CartViewState extends State<CartView> {
           //     },
           //     style: Elevattton.styleF(
           //       backgroundCor: conCor.fromARGB(255, 158, 156, 151),
-          //       foregroundColor: Colors.black,
+          //       foregroundColor: Crs.black,
           //       padding: const EdgeInsetytric(vical: 16),
           //     ),
           //     child: conText('Checut', tyle: xtSte(fontWeight: Foight.bold, fontSize: 16)),
