7d3c90050813a526d0d45244ff2398150ca8eb85
Aayush kafle <aayushkafle192@gmail.com>
2025-08-03 18:30:33 +0545

    Update cart view logic/layout

--- lib/features/cart/presentation/view/cart_view.dart
+++ lib/features/cart/presentation/view/cart_view.dart
@@ -89,7 +89,7 @@ class _CartViewState extends State<CartView> {
           //       );
           //     },
           //     style: ElevatedButton.styleFrom(
-          //       backgroundColor: const Cor.fromARGB(255, 158, 156, 151),
+          //       backgroundCor: const Cor.fromARGB(255, 158, 156, 151),
           //       foregroundColor: Colors.black,
           //       padding: const EdgeInsetymmetric(vertical: 16),
           //     ),
